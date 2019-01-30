fbafb394109026b88fcc4a449675481bd44e834c
Jamie Countryman <Jamie.Countryman@gmail.com>
2019-01-29 23:23:37 -0600

    Use mongoid config for mongofs

--- lib/mongo_fs.rb
+++ lib/mongo_fs.rb
@@ -1,6 +1,4 @@
 class MongoFs
-  DATABASE_NAME = ENV['PBOT_FILE_STORE_DB'] || 'pbot_files'
-
   def self.fs
     @mongo_fs ||= Mongo::Grid::FSBucket.new(database)
   end
@@ -8,10 +6,7 @@ class MongoFs
   private
 
   def self.database
-    @mongo_files_db ||= Mongo::Database.new(client, DATABASE_NAME)
-  end
-
-  def self.client
-    @mongo_client ||= Mongoid.default_client
+    # from mongoid.yml
+    @mongo_files_db ||= Mongoid.default_client.database
   end
 end
